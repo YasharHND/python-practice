c7f45ea554a8c3595f9a1519b4c885b5c9eda362
YasharHND <yashar@sandboxapp.io>
2022-11-02 17:56:55 +0300

    iterator

--- iterator.py
+++ iterator.py
@@ -1,3 +1,6 @@
+import os
+
+
 class NaturalEvenNumbers:
     def __init__(self, limit):
         self.limit = limit
@@ -16,3 +19,12 @@ class NaturalEvenNumbers:
 
 for i in NaturalEvenNumbers(10):
     print(i)
+
+
+# Sample using iterator
+# Listing directories and files under the current working directory
+path = os.getcwd()
+_, dir_names, file_names = next(iter(os.walk(path)))
+
+print("directories:", dir_names)
+print("files:", file_names)
